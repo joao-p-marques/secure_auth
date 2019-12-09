7bb6d6b874d1fd56eee9f069fc47fe370662ab38
Tom1k <tomascosta@ua.pt>
2019-12-09 22:18:15 +0000

    added smart_Card and certs

---
+++ certificates/code.py
@@ -0,0 +1,46 @@
+
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+
+from os import scandir
+import datetime
+
+def load_certificate(file_name): 
+    now = datetime.datetime.now()
+
+    with open(file_name, 'rb') as f:
+        pem_data = f.read()
+        cert = x509.load_pem_x509_certificate(pem_data, default_backend())
+
+    print(f"Loaded {cert.serial_number}")
+    print(f"Valid from {cert.not_valid_before} to {cert.not_valid_after}", end="")
+
+    if cert.not_valid_after > now:
+        print(" EXPIRED", end="")
+
+    print()
+
+def build_issuers(chain, cert):
+    chain.append(cert)
+
+    issuer = cert.issuer.rfc4514_string()
+    subject = cert.subject.rfc4514_string()
+
+    if issuer == subject and subject in roots:
+        print("Chain completed")
+        return True
+
+    if issuer in roots:
+        return build_issuers(chain, roots[issuer])
+
+    if issuer in intermediate_certs:
+        return build_issuers(chain, intermediate_certs[issuer])
+
+
+
+for entry in scandir('/etc/ssl/certs'):
+    # print(entry.name)
+    if entry.is_dir():
+        continue
+    if not 'trust' in entry.name:# and 'crt' in entry.name:
+        load_certificate(entry)
