4fccdd686424829830399cda12b3e89a6f3bc4a4
Tom1k <tomascosta@ua.pt>
2019-12-12 21:59:40 +0000

    baseline for code

--- comms/client.py
+++ comms/client.py
@@ -70,48 +70,8 @@ class ClientProtocol(asyncio.Protocol):
         logger.debug('Connected to Server')
 
         self.state = STATE_OPEN
-        self.create_key_pair()
-        #self.authenticate_user()
+        self.authenticate_user()
         self.negotiate_algos()
-
-    def create_key_pair(self):
-        # Generate our key
-        key = rsa.generate_private_key(
-            public_exponent=65537,
-            key_size=2048,
-            backend=default_backend()
-        )
-        # Write our key to disk for safe keeping
-        with open("not_the_keys/priv_key.pem", "wb") as f:
-            f.write(key.private_bytes(
-                encoding=Encoding.PEM,
-                format=PrivateFormat.TraditionalOpenSSL,
-                encryption_algorithm=BestAvailableEncryption(b"passphrase"),
-            ))
-        #then gen csr
-        self.generate_csr(key)
-
-    def generate_csr(self,key):
-        csr = x509.CertificateSigningRequestBuilder().subject_name(x509.Name([
-            # Provide various details about who we are.
-            x509.NameAttribute(NameOID.COUNTRY_NAME, u"US"),
-            x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"California"),
-            x509.NameAttribute(NameOID.LOCALITY_NAME, u"San Francisco"),
-            x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"My Company"),
-            x509.NameAttribute(NameOID.COMMON_NAME, u"mysite.com"),
-        ])).add_extension(
-            x509.SubjectAlternativeName([
-                # Describe what sites we want this certificate for.
-                x509.DNSName(u"mysite.com"),
-                x509.DNSName(u"www.mysite.com"),
-                x509.DNSName(u"subdomain.mysite.com"),
-            ]),
-            critical=False,
-        # Sign the CSR with our private key.
-        ).sign(key, hashes.SHA256(), default_backend())
-        # Write our CSR out to disk.
-        with open("path/to/csr.pem", "wb") as f:
-            f.write(csr.public_bytes(Encoding.PEM))
 
     def authenticate_user(self):
         self.username = input("User: ")
