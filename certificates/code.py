504e2777dbe465b42c6c39f5629189ca49247c6b
joao-p-marques <joao.marques.contacto@gmail.com>
2019-12-12 12:03:30 +0000

    certs validated from webserver

--- certificates/code.py
+++ certificates/code.py
@@ -16,18 +16,22 @@ def load_certificate(file_name):
         cert = x509.load_pem_x509_certificate(pem_data, default_backend())
 
     # print(f"Loaded {cert.serial_number}")
-    # print(f"Valid from {cert.not_valid_before} to {cert.not_valid_after}", end="")
+    # print(f"Valid from {cert.not_valid_before} to {cert.not_valid_after}")
 
-    if cert.not_valid_after > now:
-        # print(" EXPIRED", end="")
-        return
+    if cert.not_valid_after < now:
+        # print(file_name, "EXPIRED (", cert.not_valid_after, ')') 
+        return cert, False
     else:
-        return cert
+        return cert, True
 
-    # print()
+    # return cert, True
 
-def build_issuers(chain, cert):
+def build_issuers(chain, cert, depth=0):
+    if depth > 10:
+        return
+
     chain.append(cert)
+    print(chain)
 
     issuer = cert.issuer.rfc4514_string()
     subject = cert.subject.rfc4514_string()
@@ -37,24 +41,28 @@ def build_issuers(chain, cert):
         return True
 
     if issuer in roots:
-        return build_issuers(chain, roots[issuer])
+        return build_issuers(chain, roots[issuer], depth+1)
+    elif issuer in intermediate_certs:
+        print('found issuer')
+        return build_issuers(chain, intermediate_certs[issuer], depth+1)
 
-    if issuer in intermediate_certs:
-        return build_issuers(chain, intermediate_certs[issuer])
+def load_certificates(dir_name, roots, intermediate_certs):
+    for entry in scandir(dir_name):
+        if entry.is_dir() or not ('pem' in entry.name or 'crt' in entry.name):
+            continue
+        c, valid = load_certificate(entry)
+        if not valid:
+            continue
+        # print('Loading', entry.name, '(', c.subject.rfc4514_string(), ')')
+        if any(x in entry.name for x in ['Root', 'ROOT', 'Trust', 'TRUST']): # and 'crt' in entry.name:
+            roots[c.subject.rfc4514_string()] = c
+        else:
+            intermediate_certs[c.subject.rfc4514_string()] = c
 
 
+load_certificates('/etc/ssl/certs', roots, intermediate_certs)
+load_certificates('.', roots, intermediate_certs)
 
-for entry in scandir('/etc/ssl/certs'):
-    # print(entry.name)
-    if entry.is_dir():
-        continue
-    c = load_certificate(entry)
-    if c is None:
-        continue
-    if 'trust' in entry.name: # and 'crt' in entry.name:
-        roots[c.issuer.rfc4514_string()] = c
-    else:
-        intermediate_certs[c.issuer.rfc4514_string()] = c
+c, valid = load_certificate('gitlab.pem')
 
-print(roots)
-print(intermediate_certs)
+build_issuers([], c)
